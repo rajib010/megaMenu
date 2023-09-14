e9c35849af674347b97eacf9a7ebcf1667e801a6
Rajib Pokhrel <pokhrelrajib015@gmail.com>
2023-09-14 13:59:15 +0545

    updates

--- megaMenu-1/js/index.js
+++ megaMenu-1/js/index.js
@@ -1,13 +1,10 @@
 'use strict';
 
 var navLinks = document.querySelector('.nav-links');
+const iconX= document.querySelector(".icon2");
 document.getElementById('menuIcon').addEventListener('click', function() {
-     
     navLinks.classList.toggle("showitem");
-    
-     
-    
-     
+    iconX.classList.toggle("showitem");
     
 });
 
